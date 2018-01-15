a5089a5404c7e754c772c676dab489a92b06c34f
vahit <vahid.maani@gmail.com>
2018-01-15 16:22:28 +0330

    resolve a bug in report conditions

--- track-pods.sh
+++ track-pods.sh
@@ -20,31 +20,30 @@ while [[ ${WAIT_TIME} != 0 ]]; do
         WAIT_TIME=1
     fi
     sleep ${WAIT_TIME}
-    COMMAND_OUTPUT1=$(kubectl --all-namespaces --output wide get pods | grep -vi "running\|namespace")
+    COMMAND_OUTPUT1=$(kubectl --all-namespaces --output wide get pods | grep -vi "running\|namespace" 2>&1)
     RETURN_CODE=${?}
-    if [[ ${RETURN_CODE} != 1 ]]; then
+    if [[ ${RETURN_CODE} != 0 && ${COMMAND_OUTPUT1} != "" ]]; then
         WAIT_TIME=$(( WAIT_TIME + 30 ))
         SUBJECT="Monitoring FAILED!!"
-        echo -e "Kubectl command could not be run completely.\n ${COMMAND_OUTPUT1}" > ${LOG_FILE}
-        COMMAND_OUTPUT2=$(EMAIL="${SENDER_NAME} <${SENDER}>" mutt -s "${SUBJECT}" -- ${RECIPIENT} < ${LOG_FILE})
+        echo -e "Kubectl command could not be run completely.\n ${COMMAND_OUTPUT1}" >> "${LOG_FILE}"
+        COMMAND_OUTPUT2=$(EMAIL="${SENDER_NAME} <${SENDER}>" mutt -s "${SUBJECT}" -- ${RECIPIENT} < "${LOG_FILE}")
         RETURN_CODE=${?}
         if [[ ${RETURN_CODE} -eq 0 ]]; then
-            rm ${LOG_FILE}
-            WAIT_TIME=0
+            rm "${LOG_FILE}"
         else
-            echo "${COMMAND_OUTPUT2}" >> ${LOG_FILE}
+            echo "${COMMAND_OUTPUT2}" >> "${LOG_FILE}"
         fi
     fi
-    if [[ $(echo ${COMMAND_OUTPUT1}) != "" ]]; then
+    if [[ $(echo ${COMMAND_OUTPUT1}) != "" && ${RETURN_CODE} == 0 ]]; then
         SUBJECT="[WARRNING] POD FAILER."
-        echo -e "One or more of our pods has a problem:\n${COMMAND_OUTPUT1}" >> ${LOG_FILE}
-        COMMAND_OUTPUT2=$(EMAIL="${SENDER_NAME} <${SENDER}>" mutt -s "${SUBJECT}" -- ${RECIPIENT} < ${LOG_FILE})
+        echo -e "One or more of our pods has a problem:\n${COMMAND_OUTPUT1}" >> "${LOG_FILE}"
+        COMMAND_OUTPUT2=$(EMAIL="${SENDER_NAME} <${SENDER}>" mutt -s "${SUBJECT}" -- ${RECIPIENT} < "${LOG_FILE}")
         RETURN_CODE=${?}
         if [[ ${RETURN_CODE} -eq 0 ]]; then
-            rm ${LOG_FILE}
+            rm "${LOG_FILE}"
             WAIT_TIME=0
         else
-            echo "${COMMAND_OUTPUT2}" >> ${LOG_FILE}
+            echo "${COMMAND_OUTPUT2}" >> "${LOG_FILE}"
         fi
     else
         WAIT_TIME=0
